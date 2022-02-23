4aced018fd36189a352ccb6ddd806b47984deac5
张晨曦 <zhangchenxi@zhangchenxideMacBook-Air.local>
2022-02-23 22:28:16 +0800

    fix:加载任务条任务下标冲突

--- ToDo/data.js
+++ ToDo/data.js
@@ -7,7 +7,6 @@ window.onload = () => {
         }
     }).then(function (response) {
         let userdata = JSON.parse(JSON.stringify(response.data));
-        console.log(userdata.emailuserinfo)
         let Arr = userdata.emailuserinfo;
         for (let i = 0; i < Arr.length; i++) {
             let { title } = Arr[i];
@@ -20,31 +19,47 @@ window.onload = () => {
         .catch(function (error) {
             console.log(error);
         });
-    axios.get('https://qct5x4.api.cloudendpoint.cn/insertItems', {
-        params: {
-            email: useremail,
-        }
-    })
-        .then(function (res) {
-            let itemInfoArr = res.data.itemInfo;
-            itemInfoArr.forEach(item => {
 
-                let { content, datastr, index, innerdex, state } = item;
+    setTimeout(() => {
+        axios.get('https://qct5x4.api.cloudendpoint.cn/insertItems', {
+            params: {
+                email: useremail,
+            }
+        })
+            .then(function (res) {
+                let itemInfoArr = res.data.itemInfo;
 
-                console.log(content, datastr, index, innerdex, state);
+                itemInfoArr.forEach(item => {
+                    let { content, datastr, index, innerdex, state } = item;
+                    /* 创建task */
+                    createtask(index, datastr, content);
+                    /* 定义任务隐藏区 */
+                    let innerarea = Array.from(areas)[index].querySelector('.hasdonearea');
 
-                console.log();
-                createtask(index, datastr, content);
-
-                if (state == 'done') {
                     let innerArr = Array.from(areas)[index].getElementsByClassName('taskitem');
-                    Array.from(innerArr)[innerdex].querySelector('.done').click();
-                }
 
+                    Array.from(innerArr)[0].setAttribute('lastdex', innerdex);
+
+                    if (state == 'done') {
+                        innerarea.style.opacity = 1;
+                        innerarea.after(Array.from(innerArr)[0]);
+                        Array.from(innerArr)[0].querySelector('.done').classList.add('change')
+                        Array.from(innerArr)[0].querySelector('.done').firstElementChild.classList.add('fontchange')
+                        Array.from(innerArr)[0].querySelector('.done').nextElementSibling.classList.add('linethrough');
+                        Array.from(innerArr)[0].setAttribute('lastdex', innerdex);
+                        Array.from(innerArr)[0].setAttribute('done', '');
+                        Array.from(lists)[index].click();
+
+                    }
+                    getalldone();
+                })
 
             })
+            .catch(function (e) {
+                console.log(e);
+            })
+    }, 0)
 
-        })
 }
 /* 设置用户名称 */
 let str = window.location.search
